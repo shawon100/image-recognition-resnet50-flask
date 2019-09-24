fe49851b23f282c1d177948422d5e76749d4e2eb
Ashadullah Shawon <shawonashadullah@gmail.com>
2019-09-24 15:28:38 +0600

    Update app.py

--- app.py
+++ app.py
@@ -83,6 +83,6 @@ if __name__ == '__main__':
     # app.run(port=5002, debug=True)
 
     # Serve the app with gevent
-    http_server = WSGIServer(('', 5000), app)
-    http_server.serve_forever()
-    #app.run()
+    #http_server = WSGIServer(('', 5000), app)
+    #http_server.serve_forever()
+    app.run()
